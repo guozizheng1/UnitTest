e1502a33c242254e2eee7f06824c318b668aa6a5
gzzwinner <gzzwinner@163.com>
2017-09-14 19:53:11 +0600

    example for unittest commit

--- test/app/example_test.py
+++ test/app/example_test.py
@@ -1,6 +1,13 @@
 import unittest
 
 
+def my_func():
+    try:
+        result = int('s')
+    except ValueError as err:
+        raise err
+
+
 class TestExample(unittest.TestCase):
     # 判断是否相等
     def test_assert_equal(self):
@@ -13,6 +20,14 @@ class TestExample(unittest.TestCase):
     # 判断是否大于
     def test_assert_greater(self):
         self.assertGreater(2, 1)
+
+    # 判断是否抛出异常
+    def test_assert_exception(self):
+        self.assertRaises(ValueError, my_func)
+
+    # 判断成员是否在序列之内
+    def test_assert_In(self):
+        self.assertIn(1, [1, 2, 3])
 
 if __name__ == '__main__':
     unittest.main()
